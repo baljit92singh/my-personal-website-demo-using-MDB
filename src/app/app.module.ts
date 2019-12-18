750ce8af6bff0956fdac21a1a16be3f86744fbd7
baljit92singh <balu.singh55@gmail.com>
2019-12-18 22:11:42 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { FlexLayoutModule } from '@angular/flex-layout';
+import { ScrollToModule } from 'ng2-scroll-to';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+
+import { MyIntroComponent } from './my-intro/my-intro.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { MyBlogComponent } from './my-blog/my-blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    MyIntroComponent,
+    MyAboutComponent,
+    MyProjectsComponent,
+    MyBlogComponent,
+    ContactComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    // FlexLayoutModule,
+    ScrollToModule.forRoot(),
+    ReactiveFormsModule
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  contactForm: FormGroup;
+  disabledSubmitButton: boolean = true;
+  optionsSelect: Array<any>;
+
+  @HostListener('input') oninput() {
+
+    if (this.contactForm.valid) {
+      this.disabledSubmitButton = false;
+    }
+  }
+
+  constructor(fb: FormBuilder,
+    // private connectionService: ConnectionService
+  ) {
+
+    this.contactForm = fb.group({
+      'contactFormName': ['', Validators.required],
+      'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+      // 'contactFormSubjects': ['', Validators.required],
+      'contactFormMessage': ['', Validators.required],
+      // 'contactFormCopy': ['', Validators.requiredTrue],
+    });
+  }
+
+  ngOnInit() {
+
+    // this.optionsSelect = [
+    //   { value: 'Feedback', label: 'Feedback' },
+    //   { value: 'Report a bug', label: 'Report a bug' },
+    //   { value: 'Feature request', label: 'Feature request' },
+    //   { value: 'Other stuff', label: 'Other stuff' },
+    // ];
+  }
+
+  get name() {
+    return this.contactForm.get('contactFormName');
+  }
+  get email() {
+    return this.contactForm.get('contactFormEmail');
+  }
+  // get subjects() {
+  //   return this.contactForm.get('contactFormSubjects');
+  // }
+  get message() {
+    return this.contactForm.get('contactFormMessage');
+  }
+  // get copy() {
+  //   return this.contactForm.get('contactFormCopy');
+  // }
+
+  onSubmit() {
+    // this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
+    //   alert('Your message has been sent.');
+    //   this.contactForm.reset();
+    //   this.disabledSubmitButton = true;
+    // }, (error: any) => {
+    //   console.log('Error', error);
+    // });
+  }
+
+}
